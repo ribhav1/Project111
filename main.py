3f4a2dc519a9dc0a8d8928024b2781c69a0ee884
ribhav1 <70613150+ribhav1@users.noreply.github.com>
2021-12-05 14:58:19 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import csv
+import random
+import statistics
+import pandas as pd
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+
+file_data = pd.read_csv('medium_data.csv')
+
+data = file_data['claps'].tolist()
+
+population_mean = statistics.mean(data)
+population_stdev = statistics.stdev(data)
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index = random.randint(0, len(data) - 1)
+        value = data[random_index]
+        dataset.append(value)
+    mean_value = statistics.mean(dataset)
+    return mean_value
+
+
+def show_fig(mean_list):
+    df = mean_list
+
+    fig = ff.create_distplot([df], ['claps'], show_hist=False)
+
+    fig.add_trace(go.Scatter(x = [population_mean, population_mean], y = [0, 0.17], mode = 'lines', name = 'mean'))
+
+    fig.add_trace(go.Scatter(x = [first_stdev_start, first_stdev_start], y = [0, 0.17], mode = 'lines', name = 'first_stdev_start'))
+    fig.add_trace(go.Scatter(x = [first_stdev_end, first_stdev_end], y = [0, 0.17], mode = 'lines', name = 'first_stdev_end'))
+
+    fig.add_trace(go.Scatter(x = [second_stdev_start, second_stdev_start], y = [0, 0.17], mode = 'lines', name = 'second_stdev_start'))
+    fig.add_trace(go.Scatter(x = [second_stdev_end, second_stdev_end], y = [0, 0.17], mode = 'lines', name = 'second_stdev_end'))
+
+    fig.add_trace(go.Scatter(x = [third_stdev_start, third_stdev_start], y = [0, 0.17], mode = 'lines', name = 'third_stdev_start'))
+    fig.add_trace(go.Scatter(x = [third_stdev_end, third_stdev_end], y = [0, 0.17], mode = 'lines', name = 'third_stdev_end'))
+
+    fig.show()
+
+    z_score_value = (statistics.mean(df) - population_mean) / (population_stdev)
+    print('Z score value: ', str(z_score_value))
+
+def setup():
+    mean_list = []
+    for i in (range(0, 100)):
+        set_of_mean = random_set_of_mean(30)
+        mean_list.append(set_of_mean)
+    show_fig(mean_list)
+
+first_stdev_start, first_stdev_end = population_mean - population_stdev, population_mean + population_stdev
+second_stdev_start, second_stdev_end = population_mean - (2 * population_stdev), population_mean + (2 * population_stdev)
+third_stdev_start, third_stdev_end = population_mean - (3 * population_stdev), population_mean + (3 * population_stdev)
+
+setup()
